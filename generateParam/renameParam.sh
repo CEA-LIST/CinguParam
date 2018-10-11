33d459f1b45bbb9c2f9da80cfbd6de76cca894a6
Vincent Herbert <vincent.herbert@caramail.com>
2018-10-11 16:07:33 +0200

    add 2018 Copyright

--- generateParam/renameParam.sh
+++ generateParam/renameParam.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# -*- coding: utf-8 -*-
+#
+#    (C) Copyright 2018 CEA LIST. All Rights Reserved.
+#    Contributor(s): Cingulata team (formerly Armadillo team)
+#
+#    This software is governed by the CeCILL-C license under French law and
+#    abiding by the rules of distribution of free software.  You can  use,
+#    modify and/ or redistribute the software under the terms of the CeCILL-C
+#    license as circulated by CEA, CNRS and INRIA at the following URL
+#    "http://www.cecill.info".
+#
+#    As a counterpart to the access to the source code and  rights to copy,
+#    modify and redistribute granted by the license, users are provided only
+#    with a limited warranty  and the software's author,  the holder of the
+#    economic rights,  and the successive licensors  have only  limited
+#    liability.
+#
+#    The fact that you are presently reading this means that you have had
+#    knowledge of the CeCILL-C license and that you accept its terms.
+#
+
 # estimated secu can be much greater than minimal security required.
 # approximated_secu is the highest multiple of 64 lower than (estimated secu + 8)
 # example : 128 is the minimum required, 203 is estimated with lwe-estimator, 192 is the approximation on estimated secu in xml filename
