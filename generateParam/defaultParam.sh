b6f60a9837ce2aa3799f0b3e476b2dc7c9bbd516
Vincent Herbert <vincent.herbert@caramail.com>
2019-02-01 13:43:55 +0100

    add shell script to generate database

---
+++ generateParam/defaultParam.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+    #(C) Copyright 2019 CEA LIST. All Rights Reserved.
+    #Contributor(s): Cingulata team (formerly Armadillo team)
+ 
+    #This software is governed by the CeCILL-C license under French law and
+    #abiding by the rules of distribution of free software.  You can  use,
+    #modify and/ or redistribute the software under the terms of the CeCILL-C
+    #license as circulated by CEA, CNRS and INRIA at the following URL
+    #"http://www.cecill.info".
+ 
+    #As a counterpart to the access to the source code and  rights to copy,
+    #modify and redistribute granted by the license, users are provided only
+    #with a limited warranty  and the software's author,  the holder of the
+    #economic rights,  and the successive licensors  have only  limited
+    #liability.
+ 
+    #The fact that you are presently reading this means that you have had
+    #knowledge of the CeCILL-C license and that you accept its terms.
+
+default_param()
+{
+        conf=$1
+        if [ ${conf} = "Cingulata_BFV" ]
+        then
+               PRIVATE_KEY_DISTRIB='0,1,63' 
+               SECURITY_REDUCTION="yes"
+                
+        elif  [ ${conf} = "SEAL_BFV" ]
+        then
+                PRIVATE_KEY_DISTRIB='" -1",1'
+                SECURITY_REDUCTION="no"
+        else
+             echo "ERROR Choose a correct value (i.e. Cingulata_BFV or SEAL_BFV) for configuration variable."
+             exit 1   
+        fi
+}
---
+++ generateParam/genDatabase.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+    #(C) Copyright 2019 CEA LIST. All Rights Reserved.
+    #Contributor(s): Cingulata team (formerly Armadillo team)
+ 
+    #This software is governed by the CeCILL-C license under French law and
+    #abiding by the rules of distribution of free software.  You can  use,
+    #modify and/ or redistribute the software under the terms of the CeCILL-C
+    #license as circulated by CEA, CNRS and INRIA at the following URL
+    #"http://www.cecill.info".
+ 
+    #As a counterpart to the access to the source code and  rights to copy,
+    #modify and redistribute granted by the license, users are provided only
+    #with a limited warranty  and the software's author,  the holder of the
+    #economic rights,  and the successive licensors  have only  limited
+    #liability.
+ 
+    #The fact that you are presently reading this means that you have had
+    #knowledge of the CeCILL-C license and that you accept its terms.
+    
+    
+HEAD_ID=$(git ls-remote https://bitbucket.org/malb/lwe-estimator.git HEAD | awk '{print $1}' | cut -c-7 )
+PARAM_DIR="../storeParam"
+PLAINTEXT_MODULUS=${1:-2} # Binary plaintext by default
+CONF=${2:-Cingulata_BFV} # Possible values: Cingulata_BGV, SEAL_BFV. More info in defaultParam.sh file.
+
+source defaultParam.sh
+
+default_param ${CONF} 
+
+parallel  --header : --results ${PARAM_DIR}/${HEAD_ID} bash updateParam.sh {1} {2} {3} {4} ${HEAD_ID} ${PLAINTEXT_MODULUS} ${PRIVATE_KEY_DISTRIB} ${SECURITY_REDUCTION} ::: mult_depth $(seq 20) ::: min_secu 80 128 192 ::: model "bkz_enum" "bkz_sieve" "core_sieve" "q_core_sieve" ::: gen_method "wordsizeinc" "bitsizeinc" && bash renameParam.sh ${PARAM_DIR}/${HEAD_ID} 80 128 192
